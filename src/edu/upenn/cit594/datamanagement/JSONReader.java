0d9bae5ae9cf2142368453abd9aa3bec45f73a4a
James <jimbotwina16@gmail.com>
2019-07-30 13:25:09 -0400

    Section2: complete aggregated fines by zip

--- src/edu/upenn/cit594/datamanagement/JSONReader.java
+++ src/edu/upenn/cit594/datamanagement/JSONReader.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 import org.json.simple.*;
 import org.json.simple.parser.JSONParser;
@@ -14,12 +16,14 @@ import org.json.simple.parser.ParseException;
 public class JSONReader {
 	JSONParser parser = new JSONParser();
 	JSONArray violations;
+	HashMap<String, Integer> finesByZip = new HashMap<>();
+	
 	public JSONReader(String parkingFile) throws FileNotFoundException, IOException, ParseException {
 		violations = (JSONArray)parser.parse(new FileReader(parkingFile));
-		
+		/*
 		JSONObject tenth = (JSONObject) violations.get(1);
 		System.out.println((tenth.get("state").toString().equals("PA")));
-		
+		*/
 	}
 	
 	public void writeZipAndFines() throws FileNotFoundException {
@@ -34,6 +38,40 @@ public class JSONReader {
 		    printWriter.println ((violationObj.get("zip_code") + " "+ violationObj.get("fine")));
 		}
 		printWriter.close();
+	}
+	
+	public HashMap<String, Integer> aggregateFinesByZip(){
 		
+		File finesFile = new File("test.txt");
+		Scanner finesScan = null;
+		try {
+			finesScan = new Scanner(finesFile);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		while(finesScan.hasNextLine()) {
+			String zipAndFineRaw = finesScan.nextLine();
+			String[] zipAndFine = zipAndFineRaw.split(" ");
+			String zip = (String) zipAndFine[0];
+			Integer fine = Integer.parseInt(zipAndFine[1]);
+				if(!finesByZip.containsKey(zip)) {
+					finesByZip.put(zip, fine);
+				}
+				else {
+					Integer currentAgg = finesByZip.get(zip);
+					finesByZip.put(zip, currentAgg += fine );
+				}
+		}
+		return finesByZip;
+	}
+	
+	public void writeTotalFile() throws FileNotFoundException {
+		PrintWriter printWriter = new PrintWriter ("total.txt");
+		for(Map.Entry<String, Integer> entry : finesByZip.entrySet()) {
+			printWriter.println (entry.getKey()+ " "+ entry.getValue());
+		}
+		printWriter.close();
 	}
 }
--- src/edu/upenn/cit594/datamanagement/Main.java
+++ src/edu/upenn/cit594/datamanagement/Main.java
@@ -10,6 +10,7 @@ public class Main {
 	public static void main(String[] args) throws FileNotFoundException, IOException, ParseException {
 		JSONReader js = new JSONReader("parking.json");
 		js.writeZipAndFines();
-		
+		js.aggregateFinesByZip();
+		js.writeTotalFile();
 	}
 }
